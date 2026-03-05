d9b0639e0fa4c4953d3f6fde15ea16502a900c8e
Sankalp Varshney <webadmin@itenmedia.in>
2026-03-05 17:08:52 +0530

    feat: Add Google Tag Manager integration and create ads.txt.

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter, Merriweather } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import SiteLayout from "@/components/layout/site-layout";
 
@@ -91,7 +92,26 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${sans.variable} ${serif.variable}`}>
+        <head>
+          {/* Google Tag Manager */}
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5P4C363M');`}
+          </Script>
+        </head>
         <body className="antialiased font-sans" suppressHydrationWarning>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5P4C363M"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           <SiteLayout>
             {children}
           </SiteLayout>
