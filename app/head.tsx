4f4314edbec1e1e4d1b2e8eed662651dae3d9f81
gijuhan <thegijuhan@gmail.com>
2026-05-04 12:24:17 +0530

    feat: Add RSS feed generation and head component

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        title="ENERGDIVE RSS Feed"
+        href="/rss.xml"
+      />
+    </>
+  );
+}
